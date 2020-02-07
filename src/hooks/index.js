b0b104c9b269a40951086cf28edf4fc589d31e01
Juan Miguel Gomez <juanmiguelgomez8@gmail.com>
2020-02-06 21:03:07 -0500

    LATIR-9: Adding firebase connection and some unrelated changes regarding favicon

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,4 +5,6 @@ export {
     isTablet,
     isMobile
 } from './useDevice';
+export { default as useAuth } from './useAuth'
+export { default as useDatabase } from './useDatabase'
 export { default as useWindowSize } from './useDevice'
