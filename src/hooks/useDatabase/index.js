a430842a1f64566d68c294f23d76c431ca8d0d8d
Juan Miguel Gomez <Jkierem@users.noreply.github.com>
2020-02-07 09:57:28 -0500

    Latir 9 (#49)
    * LATIR-9: Adding firebase connection and some unrelated changes regarding favicon
    
    * LATIR-9: Regen lock file
    
    * LATIR-9: Removing unnecesary function
    
    * LATIR-9: Fixing lockfile

---
+++ src/hooks/useDatabase/index.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import { Firebase } from '../../middleware';
+
+/**
+ * listens to a given path of the firebase realtime database
+ * @param {string} path
+ * @returns {[any]} the data as a javascript object
+ */
+const useDatabase = (path) => {
+    const [ data , setData ] = useState({})
+    useEffect(() => {
+        return Firebase.database(path)
+        .map(x => x.toJSON())
+        .map(setData)
+        .unsafeListen()
+    },[path])
+    return [data]
+}
+
+export default useDatabase;
--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,1 +1,2 @@
 // Here we should export all entities that communicate with APIs
+export { default as Firebase } from './Firebase';
