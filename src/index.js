3d1c85bdfc79c9cbaff567a9540c8b491623fc9c
Camila Vanegas <30415074+cavac96@users.noreply.github.com>
2020-02-08 17:23:18 -0500

    LATIR-34 Added i18n (#55)

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import ConfigureStore from "./store";
+import i18nInitializer from "./i18n";
 
 import "./reset.css";
 import "./index.css";
 
 const store = ConfigureStore();
+i18nInitializer(store);
 
 ReactDOM.render(
 	<Router>
