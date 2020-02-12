443950695cbb7906a1d7af78326280e9e489f238
William Baquero <williambaqueror@gmail.com>
2020-02-12 16:56:27 -0500

    LATIR-14: action button

--- src/views/WhatIsLatir/index.js
+++ src/views/WhatIsLatir/index.js
@@ -9,29 +9,10 @@ import Icon from "../../components/Icon"
 import WhatIsLatirComponent from "../../components/WhatIsLatirComponent"
  
 
-const StyleButton = styled.div`
- text-align: center;
- align-items: center;
- padding-left: 30%;
- padding-right: 30%;
-`
-const StyleImage = styled.div`
- padding-top: 20px;
- padding-bottom: 20px;
- text-align: center;
- align-items: center;
-`
-
  const WhatIsLatir = (props) => {
  return(
    <div>
-       <StyleImage><Icon src={logo} size="huge"/></StyleImage>
        <WhatIsLatirComponent/>
-       <StyleButton>
-         <Button style={{width: "90%"}}>Vision, Mision y Valores</Button>
-         <Button style={{width: "90%"}}>La Historia que Comienza</Button>
-         <Button style={{width: "90%"}}>Donde Hemos Estado</Button>
-       </StyleButton>
    </div>
  )
 }
