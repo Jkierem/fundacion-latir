8a501a631744919c107c149710fe71b507220757
Juan Felipe Castañeda <juanario1997@gmail.com>
2020-03-27 15:17:59 -0500

    LATIR-18: Where have we been view added (#65)
    * LATIR-18: Where have we been view added
    
    * LATIR-18: Added section and ID
    
    Co-authored-by: Juan Miguel Gomez <juanmiguelgomez8@gmail.com>

---
+++ src/views/WhatIsLatir/WhereHaveBeen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Section } from "../../../components";
+import Locations from "./Locations";
+
+const WhereHaveBeen = props => {
+	return (
+		<Section id="places">
+			<Locations />
+		</Section>
+	);
+};
+
+export default WhereHaveBeen;
